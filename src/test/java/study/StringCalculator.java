99a045ddb1c17dc793251d555b0bac54a9127511
hoon25 <cchoon95@hanmail.net>
2023-06-28 23:20:56 +0900

    Reformat code

--- src/test/java/study/StringCalculator.java
+++ src/test/java/study/StringCalculator.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculator {
